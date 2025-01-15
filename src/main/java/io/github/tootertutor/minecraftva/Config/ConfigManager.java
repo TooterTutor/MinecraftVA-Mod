6affe946f03c11b64488bb5d969701df393e9972
TooterTutor <tootertutor@proton.me>
2025-01-14 23:30:00 -0700

    Added Cloth Config and ModMenu Integration

---
+++ src/main/java/io/github/tootertutor/minecraftva/Config/ConfigManager.java
@@ -0,0 +1,24 @@
+package io.github.tootertutor.minecraftva.Config;
+
+import java.util.Random;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Config(name = "VoiceAttackAPI")
+@Config.Gui.Background("minecraft:/textures/block/gray_stained_glass.png")
+@Environment(EnvType.CLIENT)
+public class ConfigManager implements ConfigData {
+    public boolean randomPort = false;
+    public int port = 28643;
+
+    public int getPort() {
+        if (randomPort) {
+            Random random = new Random();
+            return random.nextInt(65535 - 1024) + 1024; // Random port between 1024 and 65535
+        }
+        return port;
+    }
+}
